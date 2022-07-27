a0ceceff164d53e98b89b712c31ca2caf4da4d59
Feeamaral <felipe.benedicto@sptech.school>
2022-07-27 00:35:01 -0300

    Encapsulando o sistema com Java

---
+++ Encapsulamento.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.encapsulamento;
+
+
+public class Encapsulamento {
+
+    public static void main(String[] args) {
+        
+        controleRemoto c = new controleRemoto();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.maisVolume();
+        c.menosVolume();
+        c.abrirMenu();
+
+        
+    }
+}
